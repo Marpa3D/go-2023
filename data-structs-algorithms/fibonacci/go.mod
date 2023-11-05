12ca00384a30f06419e731a3c6ef6ecfe4a38267
Viacheslav "Marpa3D" Sharov <Marpa3d@yandex.ru>
2023-11-05 11:03:05 +0300

    Первый способ вычисления ряда Фибоначчи. Начало

---
+++ data-structs-algorithms/fibonacci/go.mod
@@ -0,0 +1,3 @@
+module github.com/Marpa3D/go-2023/data-structs-algorithms/fibonacci
+
+go 1.21.1
--- data-structs-algorithms/fibonacci/main.go
+++ data-structs-algorithms/fibonacci/main.go
@@ -1,6 +1,12 @@
+// Ряд Фибоначчи. Несколько реализаций алгоритма
 package main
 
 import "fmt"
+
+// fibOneVersion - первая версия вычисления ряда Фибоначчи
+func fibOneVersion(num int) int {
+
+}
 
 func main() {
 	fmt.Println("Start program algorithm Fibonacci")
