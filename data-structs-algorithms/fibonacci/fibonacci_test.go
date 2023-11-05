a436e6580faa78bdd9fe95977240e212dd291543
Viacheslav "Marpa3D" Sharov <Marpa3d@yandex.ru>
2023-11-05 11:17:10 +0300

    Реализация первой версии алгоритма. Функция fibOneVersion

--- data-structs-algorithms/fibonacci/fibonacci_test.go
+++ data-structs-algorithms/fibonacci/fibonacci_test.go
@@ -2,6 +2,9 @@ package main
 
 import "testing"
 
-func Test_(t *testing.T) {
-
+func Test_fibOneVersion(t *testing.T) {
+	actual := fibOneVersion(6)
+	if actual != 8 {
+		t.Error("expected value 8 in position 6")
+	}
 }
--- data-structs-algorithms/fibonacci/main.go
+++ data-structs-algorithms/fibonacci/main.go
@@ -4,10 +4,17 @@ package main
 import "fmt"
 
 // fibOneVersion - первая версия вычисления ряда Фибоначчи
-func fibOneVersion(num int) int {
+func fibOneVersion(position int) int {
+	seq := []int{0, 1}
 
+	for len(seq) <= position {
+		seq = append(seq, seq[len(seq)-1]+seq[len(seq)-2])
+	}
+	return seq[len(seq)-1]
 }
 
 func main() {
 	fmt.Println("Start program algorithm Fibonacci")
+
+	fmt.Printf("Версия алгоритма 1. Число Фибоначчи: %d\n", fibOneVersion(6))
 }
