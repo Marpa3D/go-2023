cfc8e20f2adaddd9f2417921e149c19ab6373f42
Viacheslav "Marpa3D" Sharov <Marpa3d@yandex.ru>
2023-11-23 04:39:37 +0300

    Созданы две тестовые функции с рызным способом вывода ошибок

--- cukalos/errors/error.go
+++ cukalos/errors/error.go
@@ -1,6 +1,27 @@
 // тип данных error
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+// check - пример создания пользовательского сообщения об ошибке
+func check(a, b int) error {
+	if a == 0 && b == 0 {
+		return errors.New("Это пользовательское сообщение об ошибке")
+	}
+	return nil
+}
+
+// formattedError - сообщает об ошибке с помощью fmt.Errorf()
+func formattedError(a, b int) error {
+	if a == 0 && b == 0 {
+		return fmt.Errorf("a %d and b %d. UserID: %d", a, b, os.Getuid())
+	}
+}
+
 func main() {
 
 }
